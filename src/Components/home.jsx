51026b7201802d8bbfc12e594c1b2a4fa0d5bc1a
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2024-02-27 10:36:22 +0530

    first commit

---
+++ src/Components/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar";
+import Weather from "./weather";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="container">
+        <Weather />
+      </div>
+    </div>
+  );
+}
